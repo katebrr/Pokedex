02d19a1f6ac5df48d7637f06fc57b3fd8f8c0243
katebrr <katya.brylinska31@gmail.com>
2023-07-27 20:26:42 +0200

    Add Tab bar skeleton

--- app/src/main/java/com/katebrr/pokedex/features/pokemon/PokemonDetailScreen.kt
+++ app/src/main/java/com/katebrr/pokedex/features/pokemon/PokemonDetailScreen.kt
@@ -12,10 +12,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFrom
 import androidx.compose.foundation.layout.windowInsetsTopHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Android
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Attractions
+import androidx.compose.material.icons.filled.FamilyRestroom
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
@@ -24,6 +29,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -33,6 +42,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -46,6 +56,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.palette.graphics.Palette
 import coil.compose.SubcomposeAsyncImage
 import com.katebrr.pokedex.data.pokemons.model.PokemonDetail
+import io.ktor.util.reflect.instanceOf
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -77,8 +88,7 @@ fun PokemonDetailScreen(
 
     Column(
         Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.surface),
+            .fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -109,7 +119,15 @@ fun PokemonDetailScaffold(
         modifier = Modifier,
         topBar = { PokemonTopBar(pokemon, onBackClick) }
     ) { padding ->
-        PokemonTabBar(pokemon, Modifier.padding(padding))
+        Column(
+            Modifier
+                .padding(padding)
+                .fillMaxSize()
+        ) {
+            PokemonTabBar(pokemon)
+
+        }
+
     }
 
 }
@@ -131,7 +149,9 @@ fun PokemonTopBar(
         bgColor = color ?: 0
     }
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.surface),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -140,9 +160,12 @@ fun PokemonTopBar(
         TopAppBar(
             title = {
 
-               Row(
-                    verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Start,
-                    modifier = Modifier.padding(horizontal = 18.dp).fillMaxWidth()
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start,
+                    modifier = Modifier
+                        .padding(horizontal = 18.dp)
+                        .fillMaxWidth()
                 ) {
                     Text(
                         text = pokemon.name,
@@ -168,7 +191,8 @@ fun PokemonTopBar(
         Card(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(400.dp),
+                .height(400.dp)
+                .background(MaterialTheme.colorScheme.surface),
             shape = RoundedCornerShape(
                 topStart = 0.dp,
                 topEnd = 0.dp,
@@ -195,9 +219,79 @@ fun PokemonTopBar(
 
 @Composable
 fun PokemonTabBar(
-    pokemon: PokemonDetail,
-    modifier: Modifier
+    pokemon: PokemonDetail
 ) {
+    var tabIndex by rememberSaveable {
+        mutableStateOf(0)
+    }
+
+    val tabs = listOf("Info", "Genetics", "Attacks")
+
+    TabRow(
+        selectedTabIndex = tabIndex,
+        containerColor = MaterialTheme.colorScheme.surface,
+        contentColor = MaterialTheme.colorScheme.onSurface,
+        indicator = {
+            TabRowDefaults.Indicator(
+                Modifier.tabIndicatorOffset(it.get(tabIndex))
+            )
+        },
+        modifier = Modifier.fillMaxWidth()
+
+    ) {
+        tabs.forEachIndexed { index, title ->
+            Tab(modifier = Modifier.fillMaxSize(1f),
+                text = {
+                    Text(
+                        title,
+                        style = MaterialTheme.typography.titleSmall
+                    )
+                },
+                icon = {
+                    when (index) {
+                        0 -> Icon(imageVector = Icons.Filled.Info, contentDescription = null)
+                        1 -> Icon(imageVector = Icons.Filled.Android, contentDescription = null)
+                        2 -> Icon(imageVector = Icons.Filled.Attractions, contentDescription = null)
+                    }
+                },
+                selected = tabIndex == index,
+                selectedContentColor = MaterialTheme.colorScheme.primary,
+                unselectedContentColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                onClick = { tabIndex = index }
+            )
+        }
+    }
+
+    when (tabIndex) {
+        0 -> InfoScreen(pokemon)
+        1 -> GeneticsScreen(pokemon)
+        2 -> AttacksScreen(pokemon)
+    }
+
+
+}
+
+@Composable
+fun InfoScreen(pokemon: PokemonDetail) {
+    val pokemonStats = pokemon.stats
+    Column(Modifier.fillMaxWidth()) {
+        Text(text = pokemonStats.HP.toString())
+        Text(text = pokemonStats.attack.toString())
+        Text(text = pokemonStats.defense.toString())
+        Text(text = pokemonStats.specialAttack.toString())
+        Text(text = pokemonStats.specialDefense.toString())
+
+    }
+}
+
+
+@Composable
+fun GeneticsScreen(pokemon: PokemonDetail) {
+
+}
+
+@Composable
+fun AttacksScreen(pokemon: PokemonDetail) {
 
 }
 
