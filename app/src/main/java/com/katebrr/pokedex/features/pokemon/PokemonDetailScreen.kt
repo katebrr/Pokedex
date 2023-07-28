206e16fd561fc4626b97b94835513e339466daac
katebrr <katya.brylinska31@gmail.com>
2023-07-29 00:20:18 +0200

    Do Info Tab Screen

--- app/src/main/java/com/katebrr/pokedex/features/pokemon/PokemonDetailScreen.kt
+++ app/src/main/java/com/katebrr/pokedex/features/pokemon/PokemonDetailScreen.kt
@@ -2,19 +2,29 @@ package com.katebrr.pokedex.features.pokemon
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.ImageDecoder
+import android.util.Log
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFrom
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsTopHeight
+
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Android
 import androidx.compose.material.icons.filled.ArrowBack
@@ -24,13 +34,18 @@ import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
+import androidx.compose.material.TabRow
+import androidx.compose.material.icons.outlined.Android
+import androidx.compose.material.icons.outlined.LocalPolice
+import androidx.compose.material.icons.outlined.Shield
+import androidx.compose.material.icons.outlined.Speed
 import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
@@ -47,15 +62,26 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.core.graphics.ColorUtils
+
 
 import androidx.hilt.navigation.compose.hiltViewModel
 
 import androidx.palette.graphics.Palette
 import coil.compose.SubcomposeAsyncImage
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import com.google.accompanist.pager.rememberPagerState
+import com.katebrr.pokedex.R
 import com.katebrr.pokedex.data.pokemons.model.PokemonDetail
+import com.katebrr.pokedex.data.pokemons.model.PokemonStats
 import io.ktor.util.reflect.instanceOf
 
 import kotlinx.coroutines.Dispatchers
@@ -114,17 +140,65 @@ fun PokemonDetailScaffold(
     pokemon: PokemonDetail,
     onBackClick: () -> Unit
 ) {
+    var bgColor by remember { mutableStateOf(Color(0)) }
+
+    LaunchedEffect(Unit) {
+        // Call the suspended function from a coroutine scope
+        val color = withContext(Dispatchers.IO) {
+            getDominantColor(pokemon.image)
+        }
+        bgColor = color?.let {
+            Color(it).copy(
+                red = (Color(it).red * Color(it).red).coerceAtMost(1f),
+                green = (Color(it).green * Color(it).green).coerceAtMost(1f),
+                blue = (Color(it).blue * Color(it).blue).coerceAtMost(1f)
+            )
+        } ?: Color(0)
+    }
 
     Scaffold(
         modifier = Modifier,
-        topBar = { PokemonTopBar(pokemon, onBackClick) }
+        topBar = { PokemonTopBar(pokemon, bgColor, onBackClick) }
     ) { padding ->
         Column(
             Modifier
                 .padding(padding)
                 .fillMaxSize()
+                .background(bgColor)
         ) {
-            PokemonTabBar(pokemon)
+            Box(contentAlignment = Alignment.Center) {
+                Column {
+                    Box(
+                        Modifier
+                            .fillMaxWidth()
+                            .height(270.dp)
+                            .background(Color.Transparent)
+                    ) {
+                    }
+                    Card(
+                        modifier = Modifier.fillMaxSize(),
+                        shape = RoundedCornerShape(
+                            topStart = 30.dp,
+                            topEnd = 30.dp,
+                            bottomStart = 0.dp,
+                            bottomEnd = 0.dp
+                        ),
+                        colors = CardDefaults.cardColors(containerColor = Color.White)
+                    ) {
+                        PokemonTabBar(pokemon)
+                    }
+                }
+
+                SubcomposeAsyncImage(
+                    model = pokemon.image,
+                    contentDescription = pokemon.name,
+                    modifier = Modifier
+                        .height(250.dp)
+                        .fillMaxSize()
+                        .offset(y = -200.dp)
+                )
+            }
+
 
         }
 
@@ -137,149 +211,195 @@ fun PokemonDetailScaffold(
 @Composable
 fun PokemonTopBar(
     pokemon: PokemonDetail,
+    bgColor: Color,
     onBackClick: () -> Unit
 ) {
-    var bgColor by remember { mutableStateOf(0) }
-
-    LaunchedEffect(Unit) {
-        // Call the suspended function from a coroutine scope
-        val color = withContext(Dispatchers.IO) {
-            getDominantColor(pokemon.image)
-        }
-        bgColor = color ?: 0
-    }
-    Column(
+    TopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start,
+                modifier = Modifier
+                    .padding(horizontal = 18.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = pokemon.name,
+                    fontWeight = FontWeight.SemiBold,
+                    style = MaterialTheme.typography.headlineMedium
+                )
+            }
+        },
         modifier = Modifier
             .fillMaxWidth()
-            .background(MaterialTheme.colorScheme.surface),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-
-
-        TopAppBar(
-            title = {
-
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Start,
-                    modifier = Modifier
-                        .padding(horizontal = 18.dp)
-                        .fillMaxWidth()
-                ) {
-                    Text(
-                        text = pokemon.name,
-                        fontWeight = FontWeight.SemiBold,
-                        style = MaterialTheme.typography.headlineMedium
-                    )
-
-                }
-
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(Color(bgColor).copy(alpha = 0.4f)),
-            navigationIcon = {
-                IconButton(onClick = onBackClick) {
-                    Icon(
-                        imageVector = Icons.Filled.ArrowBack,
-                        contentDescription = "Return to poke;on list"
-                    )
-                }
-            }, colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Transparent)
+            .background(bgColor),
+        navigationIcon = {
+            IconButton(onClick = onBackClick) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = "Return to poke;on list"
+                )
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color.Transparent,
+            navigationIconContentColor = Color.White,
+            titleContentColor = Color.White
         )
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(400.dp)
-                .background(MaterialTheme.colorScheme.surface),
-            shape = RoundedCornerShape(
-                topStart = 0.dp,
-                topEnd = 0.dp,
-                bottomStart = 40.dp,
-                bottomEnd = 40.dp
-            ),
-            colors = CardDefaults.cardColors(
-                containerColor = Color(bgColor).copy(alpha = 0.4f)
-            )
-        ) {
-
-            SubcomposeAsyncImage(
-                model = pokemon.image,
-                contentDescription = pokemon.name,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(30.dp, bottom = 50.dp)
-            )
-
-        }
-    }
+    )
 
 }
+
+@OptIn(ExperimentalFoundationApi::class, ExperimentalPagerApi::class)
 
 @Composable
 fun PokemonTabBar(
     pokemon: PokemonDetail
 ) {
-    var tabIndex by rememberSaveable {
+    var tabSelected by rememberSaveable {
         mutableStateOf(0)
     }
-
     val tabs = listOf("Info", "Genetics", "Attacks")
+    var pagerState = rememberPagerState()
 
-    TabRow(
-        selectedTabIndex = tabIndex,
-        containerColor = MaterialTheme.colorScheme.surface,
-        contentColor = MaterialTheme.colorScheme.onSurface,
-        indicator = {
-            TabRowDefaults.Indicator(
-                Modifier.tabIndicatorOffset(it.get(tabIndex))
-            )
-        },
-        modifier = Modifier.fillMaxWidth()
-
-    ) {
-        tabs.forEachIndexed { index, title ->
-            Tab(modifier = Modifier.fillMaxSize(1f),
-                text = {
-                    Text(
-                        title,
-                        style = MaterialTheme.typography.titleSmall
+    Column {
+        TabRow(
+            selectedTabIndex = pagerState.currentPage,
+            backgroundColor = Color.White,
+            contentColor = MaterialTheme.colorScheme.onSurface,
+            indicator = { tabPositions -> // 3.
+                TabRowDefaults.Indicator(
+                    Modifier.pagerTabIndicatorOffset(
+                        pagerState,
+                        tabPositions
                     )
-                },
-                icon = {
-                    when (index) {
-                        0 -> Icon(imageVector = Icons.Filled.Info, contentDescription = null)
-                        1 -> Icon(imageVector = Icons.Filled.Android, contentDescription = null)
-                        2 -> Icon(imageVector = Icons.Filled.Attractions, contentDescription = null)
-                    }
-                },
-                selected = tabIndex == index,
-                selectedContentColor = MaterialTheme.colorScheme.primary,
-                unselectedContentColor = MaterialTheme.colorScheme.onSurfaceVariant,
-                onClick = { tabIndex = index }
-            )
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 34.dp)
+                .padding(horizontal = 12.dp),
+            divider = { Divider(thickness = 1.dp, color = Color.LightGray) }
+
+        ) {
+            tabs.forEachIndexed { index, title ->
+                Tab(modifier = Modifier
+                    .fillMaxWidth(1f)
+                    .padding(bottom = 6.dp),
+                    text = {
+                        Text(
+                            title,
+                            style = MaterialTheme.typography.titleSmall,
+                            fontWeight = FontWeight.SemiBold
+                        )
+                    },
+                    selected = pagerState.currentPage == index,
+                    selectedContentColor = Color.DarkGray,
+                    unselectedContentColor = Color.LightGray,
+                    onClick = { tabSelected = index }
+                )
+            }
+        }
+        HorizontalPager(
+            count = 3,
+            state = pagerState
+        ) { tabIndexPager ->
+//            tabIndex = tabIndexPager
+//            Log.e("horizontal pager", "${tabIndex} @ ${tabIndexPager}")
+            when (pagerState.currentPage) {
+                0 -> InfoScreen(pokemon)
+                1 -> GeneticsScreen(pokemon)
+                2 -> AttacksScreen(pokemon)
+            }
         }
     }
-
-    when (tabIndex) {
-        0 -> InfoScreen(pokemon)
-        1 -> GeneticsScreen(pokemon)
-        2 -> AttacksScreen(pokemon)
+    LaunchedEffect(tabSelected) {
+        pagerState.animateScrollToPage(tabSelected)
     }
-
-
 }
 
 @Composable
 fun InfoScreen(pokemon: PokemonDetail) {
-    val pokemonStats = pokemon.stats
-    Column(Modifier.fillMaxWidth()) {
-        Text(text = pokemonStats.HP.toString())
-        Text(text = pokemonStats.attack.toString())
-        Text(text = pokemonStats.defense.toString())
-        Text(text = pokemonStats.specialAttack.toString())
-        Text(text = pokemonStats.specialDefense.toString())
+    val pokStats = pokemon.stats
+    val total = pokStats.HP + pokStats.attack + pokStats.defense + pokStats.specialAttack + pokStats.specialDefense + pokStats.speed
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        StatsItem(statName = "HP", stat = pokStats.HP, painterResource(id = R.drawable.ecg_heart) )
+        StatsItem(statName = "Attack", stat = pokStats.attack,  painterResource(id = R.drawable.swords))
+        StatsItem(statName = "Defense", stat = pokStats.defense, painterResource(id = R.drawable.shield))
+        StatsItem(statName = "SP Attack", stat = pokStats.specialAttack, painterResource(id = R.drawable.sword_rose))
+        StatsItem(statName = "SP Defense", stat = pokStats.specialDefense, painterResource(id = R.drawable.local_police))
+        StatsItem(statName = "Speed", stat = pokStats.speed, painterResource(id = R.drawable.speed))
+        Divider(Modifier.padding(vertical = 10.dp))
+        StatsItem(statName = "Total", stat = total, icon = painterResource(id = R.drawable.android), rangeMax = 680)
+
+
+    }
+}
+
+@Composable
+fun StatsItem(
+    statName: String,
+    stat: Int,
+    icon: Painter,
+    rangeMax: Int = 160
+) {
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 10.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+
+        Row(
+            modifier = Modifier.fillMaxWidth(0.3f),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Icon(icon , contentDescription = null, Modifier.size(24.dp))
+            Text(
+                text = statName,
+                modifier = Modifier
+                    .width(160.dp)
+                    .padding(start = 4.dp),
+                color = Color.Gray,
+               // fontWeight = FontWeight.SemiBold,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+
+
+        Row(
+            modifier = Modifier.fillMaxWidth(0.9f), verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Text(
+                text = stat.toString(),
+                modifier = Modifier.width(40.dp),
+                color = Color.DarkGray,
+                fontWeight = FontWeight.SemiBold,
+                style = MaterialTheme.typography.bodyLarge,
+                textAlign = TextAlign.End
+            )
+            LinearProgressIndicator(
+                progress = stat.toFloat() / rangeMax,
+                modifier = Modifier
+                    .width(300.dp)
+                    .padding(start = 8.dp),
+                color = if (stat < rangeMax/2) MaterialTheme.colorScheme.primary else Color.hsl(
+                    149f,
+                    0.871f,
+                    0.316f
+                ),
+                backgroundColor = Color.LightGray
+            )
+
+        }
+
 
     }
 }
@@ -287,12 +407,24 @@ fun InfoScreen(pokemon: PokemonDetail) {
 
 @Composable
 fun GeneticsScreen(pokemon: PokemonDetail) {
-
+    Column(
+        Modifier
+            .fillMaxSize()
+            .background(Color.Red)
+    ) {
+        Text("tHINGS")
+    }
 }
 
 @Composable
 fun AttacksScreen(pokemon: PokemonDetail) {
-
+    Column(
+        Modifier
+            .fillMaxSize()
+            .background(Color.Green)
+    ) {
+        Text("Difefriojhfn tHINGS")
+    }
 }
 
 
@@ -321,5 +453,6 @@ suspend fun getDominantColor(imageURL: String): Int? {
         }
     }
 }
+
 
 
